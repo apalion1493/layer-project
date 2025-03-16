2717c9ee18e152669ca9fb9f020fe93b96fec497
Alexandr Petrovich <apalion97@gmail.com>
2025-03-16 21:53:13 +0200

    Add Accurate-Results.svg to project assets
    This commit introduces a new SVG file, Accurate-Results.svg, to the project assets directory. It will likely be used for UI or branding purposes.

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const swiper = new Swiper(".mySwiper", {
         slidesPerView: 4,
-        spaceBetween: 32,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-        },
+        spaceBetween: 20,
         breakpoints: {
             0: {
-                slidesPerView: 1,
+                slidesPerView: 'auto',
             },
             769: {
-                slidesPerView: 2,
+                slidesPerView: 'auto',
             },
             1025: {
                 slidesPerView: 3,
@@ -27,106 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
-
-    const swiper2 = new Swiper(".gallery-swiper", {
-        spaceBetween: 24,
-        slidesPerView: 3,
-        freeMode: true,
-        watchSlidesProgress: true,
-        loop: true,
-        breakpoints: {
-            1024: {
-                spaceBetween: 24,
-            },
-            100: {
-                spaceBetween: 16,
-            }
-        }
-    });
-
-    const swiper3 = new Swiper(".gallery-swiper-big", {
-        spaceBetween: 10,
-        zoom: true,
-        loop: true,
-        thumbs: {
-            swiper: swiper2,
-        },
-    });
-
-    const swiper4 = new Swiper(".swiper-blog", {
-        slidesPerView: 3,
-        spaceBetween: 32,
-        breakpoints: {
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 32,
-            },
-            768: {
-                slidesPerView: "auto",
-            },
-            640: {
-                slidesPerView: "auto",
-            },
-            100: {
-                slidesPerView: "auto",
-                spaceBetween: 20,
-            }
-        }
-    });
-
-    const swiper5 = new Swiper('.swiper-brands', {
-        slidesPerView: 8,
-        spaceBetween: 32,
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            1345: {
-                slidesPerView: 8,
-            },
-            1024: {
-                slidesPerView: 6,
-            },
-            768: {
-                slidesPerView: "auto",
-            },
-            640: {
-                slidesPerView: "auto",
-            },
-            100: {
-                slidesPerView: "auto",
-            }
-        }
-    });
-
-    const swiper6 = new Swiper('.checkboxes-alphabet-list--swiper', {
-        slidesPerView: "auto",
-        spaceBetween: 8,
-    })
-
-    const conditionSwiper = new Swiper(".condition-swiper", {
-        slidesPerView: 4,
-        spaceBetween: 32,
-        breakpoints: {
-            1024: {
-                slidesPerView: 4,
-            },
-            768: {
-                slidesPerView: 3,
-            },
-            640: {
-                slidesPerView: 2,
-            },
-            100: {
-                slidesPerView: "auto",
-            }
-        }
-    });
-
-
-
 })
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -142,31 +34,31 @@ document.addEventListener('DOMContentLoaded', function () {
     burgerButton?.addEventListener('click', toggleMenu);
 })
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const wrapperForm = document.getElementById("wrapperForm");
-//     const form = document.getElementById("contactForm");
-//     const thankYouMessage = document.getElementById("thankYouMessage");
-//     const resetButton = document.getElementById("resetFormButton");
-//
-//     form.addEventListener("submit", function (event) {
-//         event.preventDefault();
-//         const formData = new FormData(form);
-//         axios.post("http://localhost:3000/send-form", formData)
-//             .then(response => {
-//                 console.log("Success:", response.data);
-//                 form.reset();
-//                 wrapperForm.classList.add("hidden");
-//                 thankYouMessage.classList.remove("hidden");
-//             })
-//             .catch(error => {
-//                 console.error("Ошибка:", error);
-//             });
-//     });
-//     resetButton.addEventListener("click", function () {
-//         thankYouMessage.classList.add("hidden");
-//         wrapperForm.classList.remove("hidden");
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+    const wrapperForm = document.getElementById("wrapperForm");
+    const form = document.getElementById("contactForm");
+    const thankYouMessage = document.getElementById("thankYouMessage");
+    const resetButton = document.getElementById("resetFormButton");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        axios.post("http://localhost:3000/send-form", formData)
+            .then(response => {
+                console.log("Success:", response.data);
+                form.reset();
+                wrapperForm.classList.add("hidden");
+                thankYouMessage.classList.remove("hidden");
+            })
+            .catch(error => {
+                console.error("Ошибка:", error);
+            });
+    });
+    resetButton.addEventListener("click", function () {
+        thankYouMessage.classList.add("hidden");
+        wrapperForm.classList.remove("hidden");
+    });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     const wrapperForm = document.getElementById("wrapperForm");
@@ -200,7 +92,3 @@ document.addEventListener("DOMContentLoaded", function () {
         form_img.classList.remove("lg:hidden");
     });
 });
-
-
-
-// TODO test
